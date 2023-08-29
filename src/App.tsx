222b46e6290b5c86bea2bcb7fc5d32161a3dba3f
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2023-08-29 20:09:28 +0200

    added react-flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ReactFlow, { Background } from 'reactflow';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+import 'reactflow/dist/style.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ width: '100%', position: 'absolute', height: '100%', border: '1px solid blue'}}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} >
+        <Background color="#aaa" gap={16} />
+      </ReactFlow>
+    </div>
   )
 }
 
