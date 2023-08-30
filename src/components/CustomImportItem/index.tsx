8f7063a1cb23447cf65582924ee62fa9c4093f55
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2023-08-30 09:24:15 +0200

    added react-flow

--- src/components/CustomImportItem/index.tsx
+++ src/components/CustomImportItem/index.tsx
@@ -11,16 +11,20 @@ export interface CustomImportItemProps {
 
 export const CustomImportItem = ({object} : {object: CustomImportItemProps}) => {
   return (
-    <Paper style={{ maxWidth: '450px'}}>
+    <Paper style={{ maxWidth: '450px', overflow: 'hidden'}}>
       <Box sx={{ p: 2 }}>
         <Box display={'flex'} sx={{gap: '8px'}} alignItems={'center'}>
           <Typography variant="caption"> {object.id}</Typography>
           <Typography variant="subtitle2"> {object.label}</Typography>
         </Box>
         <Box display={'flex'} sx={{gap: '8px'}} alignItems={'flex-start'}>
-          <img src="https://via.placeholder.com/150" alt="placeholder" />
-        <Typography variant="caption" color="textSecondary"> {object.description}</Typography>
-
+          {/* <img src="https://via.placeholder.com/150" alt="placeholder" /> */}
+          <div style={{ width: '350px', height: '350px', position: 'relative', overflow: 'hidden', display: 'contents'}}>
+            <a-scene key={object.id} id={object.id}>
+              <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+            </a-scene>
+        </div>
+          <Typography variant="caption" color="textSecondary"> {object.description}</Typography>
         </Box>
       </Box>
     </Paper>
