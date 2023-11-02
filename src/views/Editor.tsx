1ab3f5cf4348b8eb64e49630cf44b36d112efecf
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2023-11-02 12:13:09 +0100

    added router first

--- src/views/Editor.tsx
+++ src/views/Editor.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Box, Checkbox, TextField, Typography } from '@mui/material';
+import { Box, Button, Checkbox, Chip, TextField, Typography } from '@mui/material';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import 'reactflow/dist/style.css';
@@ -9,6 +9,7 @@ import { UpdateEntity } from '../data/UpdateEntity';
 export default function Editor() {
   const [entity, setEntity] = useState<any>(null);
   const [needsUpdate, setNeedsUpdate] = useState<any>(false);
+  const [variable, setVariable] = useState<any>({});
   const { id } = useParams<any>();
 
   if (!id) return null
@@ -28,6 +29,49 @@ export default function Editor() {
           }} />
           <Typography>Pure component</Typography>
         </Box>
+        <Box display={'flex'} alignItems={'center'}>
+          <Checkbox checked={entity.variables || false} onChange={(e) => {
+            setEntity({...entity, variables: e.target.checked, variableList: []})
+            setNeedsUpdate(true)
+          }} />
+          <Typography>Has variables</Typography>
+        </Box>
+
+
+        <Box display={'flex'} alignItems={'center'}>
+          {entity.variableList.map(variableItem => <Chip
+            key={variableItem.name}
+            variant='outlined'
+            label={`${variableItem.name} : ${variableItem.value}`}
+            onDelete={() => {
+              setEntity({...entity, variableList: entity.variableList.filter((item: any) => item.name !== variableItem.name)})
+              setNeedsUpdate(true)
+            }}
+          />)}
+          <Box display='flex' sx={{ gap: '8px'}} alignItems={'center'}>
+            <TextField
+            placeholder="name"
+            size="small"
+            value={variable.name}
+            onChange={(e) => setVariable({...variable, name: e.target.value})}
+            />
+            <TextField
+            placeholder="value"
+            size="small"
+            value={variable.value}
+            onChange={(e) => setVariable({...variable, value: e.target.value})}
+            />
+            {/* <TextField placeholder="value" size="small" /> */}
+            <Button
+            variant='contained'
+            onClick={() => {
+              setEntity({...entity, variableList: [...entity.variableList, variable]})
+              setVariable({})
+              setNeedsUpdate(true)
+            }}
+            >add</Button>
+          </Box>
+        </Box>
 
         <TextField
           placeholder='Title'
@@ -40,22 +84,38 @@ export default function Editor() {
           }}
         />
 
+
         <TextField
           autoFocus
           value={entity.body}
           multiline={true}
-          rows={40}
+          rows={10}
+          fullWidth={true}
+          onChange={(e) => {
+            setEntity({...entity, body: e.target.value, processedBody: e.target.value})
+            setNeedsUpdate(true)
+          }}
+        />
+
+        <TextField
+          autoFocus
+          disabled
+          value={entity.processedBody}
+          multiline={true}
+          rows={10}
           fullWidth={true}
           onChange={(e) => {
             setEntity({...entity, body: e.target.value})
             setNeedsUpdate(true)
           }}
         />
+
+
       </Box>
 
       <Box style={{ flex: '0 0 50%', height: '100%', overflow: 'hidden'}} sx={{ border: '1px solid', borderRadius: '8px'}}>
         <a-scene embedded>
-          <a-box dangerouslySetInnerHTML={{ __html: entity.body }} />
+          <a-box dangerouslySetInnerHTML={{ __html: entity.processedBody }} />
         </a-scene>
       </Box>
     </Box>}
